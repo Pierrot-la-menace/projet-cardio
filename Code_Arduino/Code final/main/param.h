bdece0f307493c6f9b3b18904bcbb45592ad18a7
Royk4 <45061713+Royk4@users.noreply.github.com>
2018-11-15 21:56:21 +0100

    Update param.h

--- Code_Arduino/Code final/main/param.h
+++ Code_Arduino/Code final/main/param.h
@@ -1,3 +1,3 @@
-#define OPTION 1
-#define autre 5
+#define OPTION 1 //Ligne permettant de choisir le mode d'éclairage des LEDs
+#define autre 5 //Permet de choisir la LED a allumer pour le mode d'éclairage des LEDs n°5
 

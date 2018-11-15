c494b9a6ffd3bdec388004f33650a1d85d8d0364
Royk4 <45061713+Royk4@users.noreply.github.com>
2018-11-15 22:17:18 +0100

    Update main.c

--- Code_Arduino/Code final/Génération de code/main.c
+++ Code_Arduino/Code final/Génération de code/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "menu.h"
+#include "menu.h"// inclue les prototypes des fonctions
 #include "GenerationCode.h"
 
 int main()
 {
-    int result = 0;
-    int autre = 0;
+    int result = 0;//Variable contenant la valeur du mode d'éclairage des LEDs que l'utilisateur a choisi
+    int autre = 0;/*Variable contenant la valeur de la LED allumée que l'utilisateur a choisi si celui-ci
+                    a choisi le mode d'éclairage n°5*/
 
-    system("chcp 65001");
-    system("CLS");
+    system("chcp 65001");//Commande qui permet d'encoder les accents
+    system("CLS");//Commande qui permet d'effacer les lignes précédentes
 
-    DessinCoeur();
+    DessinCoeur();//Fonction qui permet d'afficher le dessin du coeur et le dessin du menu
 
     result = choisirmenu();
 
@@ -19,11 +20,13 @@ int main()
     {
         printf("Quelle LED veux-tu allumer ? ");
         scanf("%d", &autre);
-        parametres(result, autre);
+        parametres(result, autre);/*Fonction permettant d'écrire dans le param.h les instructions nécessaires si
+                                    l'utilisateur a choisi le mode d'éclairage n°5*/
     }
     else
     {
-        parametre(result);
+        parametre(result);/*Fonction permettant d'écrire dans le param.h les instructions nécessaires si
+                                    l'utilisateur a choisi le mode d'éclairage n°1 2 3 ou 4*/
     }
 
 
